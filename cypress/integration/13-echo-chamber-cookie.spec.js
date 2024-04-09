87834c9e4a325e89868b41089fa0177c4eb03e3c
VI0L3TF0X <mattex15243@gmail.com>
2024-04-09 16:53:39 +0200

    Cookies exercise

--- cypress/integration/13-echo-chamber-cookie.spec.js
+++ cypress/integration/13-echo-chamber-cookie.spec.js
@@ -21,12 +21,19 @@ describe('Signing in with a seeded database', () => {
     cy.location('pathname').should('contain', '/echo-chamber/posts');
   });
 
-  it('should set a cookie', () => {});
+  it('should set a cookie', () => {
+    cy.getCookie('jwt').then(cookie => {
+        const val = decodeToken(cookie.value);
+        expect(val.email).to.equal(user.email);
+    })
+  });
 });
 
 describe('Setting the cookie', () => {
+    const sampleUser = { id: 999, email: 'cypress@example.com' };
   beforeEach(() => {
     cy.task('seed');
+    cy.setCookie('jwt', encodeToken(sampleUser))
     cy.visit('/echo-chamber/sign-in');
   });
 
@@ -34,12 +41,19 @@ describe('Setting the cookie', () => {
     cy.location('pathname').should('contain', '/echo-chamber/posts');
   });
 
-  it('show that user on the page', () => {});
+  it('show that user on the page', () => {
+    cy.contains(user.email)
+  });
 });
 
 describe('Setting the cookie with real data', () => {
-  beforeEach(() => {
+  let user;
+    beforeEach(() => {
     cy.task('seed');
+      cy.request('echo-chamber/api/users').then(res => {
+        [user] = res.body.users;
+          cy.setCookie('jwt', encodeToken(user));
+      })
     cy.visit('/echo-chamber/sign-in');
   });
 
@@ -47,5 +61,7 @@ describe('Setting the cookie with real data', () => {
     cy.location('pathname').should('contain', '/echo-chamber/posts');
   });
 
-  it('show that user on the page', () => {});
+  it('show that user on the page', () => {
+    cy.contains(user.email);
+  });
 });
